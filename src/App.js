ac8d6daf090ba01fa8d4a485cc133b23e6c80660
Leo Castro <leocastro98@outlook.com>
2021-10-05 12:04:52 -0300

    add routes and shit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import './index.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
 
-const title = 'Welcome to the new Blog';
-const likes = 50;
-
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
-          <Home />
-      
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route exact path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+          </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-
+import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
@@ -6,9 +6,8 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
-                
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
      );
